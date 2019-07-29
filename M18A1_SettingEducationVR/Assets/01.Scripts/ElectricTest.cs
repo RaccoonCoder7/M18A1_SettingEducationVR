aec3a3cf9671f1d06ca27949cce94ea74c3851ad
LEE BYUNGNAM <bol0615@naver.com>
2019-07-29 14:49:55 +0900

    이펙트 추가 & 지뢰, 낙엽추가

---
+++ M18A1_SettingEducationVR/Assets/01.Scripts/ElectricTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstick))
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = new Color(212, 114, 0);
+            StartCoroutine("ColorChange");
+        }
+    }
+
+    IEnumerator ColorChange()
+    {
+        yield return new WaitForSeconds(2.0f);
+        gameObject.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0);
+    }
+}
---
+++ M18A1_SettingEducationVR/Assets/01.Scripts/FireMine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMine : MonoBehaviour
+{
+    private ParticleSystem fireParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireParticle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstick)){
+            fireParticle.Play();
+        }
+    }
+}
