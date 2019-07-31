f339ef91f0c582f08b8390cbb982ab2bcba8bcf6
DK_O <odk1193@gmail.com>
2019-07-31 16:01:24 +0900

    병합 부탁드립니다 병남

--- M18A1_SettingEducationVR/Assets/01.Scripts/Claymore.cs
+++ M18A1_SettingEducationVR/Assets/01.Scripts/Claymore.cs
@@ -4,49 +4,67 @@ using UnityEngine;
 
 public class Claymore : MonoBehaviour
 {
+    private int leaf;
     private bool isConnected;
+    private bool isSet;
     private GameObject connectedObj;
     private AudioSource audio;
-    private AudioSource audioFire;
     private Transform anchor;
+    private Ray ray;
+    private RaycastHit hit;
+    private GameObject setPointObj;
+    private List<Transform> rayPoints = new List<Transform>();
+    private List<MeshRenderer> cylinders = new List<MeshRenderer>();
     public OVRGrabber[] grabbers;
     public ParticleSystem fireParticle;
     public AudioClip separate;
     public AudioClip fire;
-
     public Connect detonatorConn;
     public Connect electricTestConn;
 
     // Start is called before the first frame update
     void Start()
     {
+        leaf = 1 << LayerMask.NameToLayer("LEAF");
         audio = GetComponent<AudioSource>();
-        audioFire = GetComponent<AudioSource>();
         anchor = transform.Find("Anchor");
+        Transform rays = transform.Find("Rays");
+        foreach (Transform child in rays)
+        {
+            MeshRenderer rend = child.Find("Cylinder").GetComponent<MeshRenderer>();
+            rend.enabled = false;
+            cylinders.Add(rend);
+            rayPoints.Add(child);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (detonatorConn.isConnected && this.isConnected && !electricTestConn.isConnected)
+        if (isSet)
         {
-            Debug.Log("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY   fire");
-
-            if (OVRInput.GetDown(OVRInput.Button.SecondaryThumbstick) && isConnected)
+            if (!CheckClaymoreHidden())
             {
-                fireParticle.Play();
-                audioFire.PlayOneShot(fire);
+                return;
+            }
+
+            if (detonatorConn.isConnected && this.isConnected
+                && !electricTestConn.isConnected && setPointObj != null)
+            {
+                if (OVRInput.GetDown(OVRInput.Button.SecondaryThumbstick))
+                {
+                    fireParticle.Play();
+                    audio.PlayOneShot(fire);
+                    Destroy(setPointObj);
+                    setPointObj = null;
+                }
             }
         }
-        else
-        {
-            Debug.Log("NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN   fire");
-        }
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isConnected) return;
         if (other.tag == "ROPE")
         {
             if (other.transform.parent == null
@@ -61,10 +79,28 @@ public class Claymore : MonoBehaviour
                 }
                 isConnected = true;
                 connectedObj = other.gameObject;
+                connectedObj.GetComponent<BoxCollider>().enabled = false;
                 connectedObj.transform.position = anchor.position;
                 connectedObj.transform.parent = anchor.parent;
                 audio.Play();
             }
+        }
+        if (other.tag == "SETPOINT")
+        {
+            setPointObj = other.gameObject;
+            setPointObj.GetComponent<MeshRenderer>().enabled = false;
+            setPointObj.GetComponent<SphereCollider>().enabled = false;
+            OVRGrabbable grabbable = transform.parent.GetComponent<OVRGrabbable>();
+            foreach (OVRGrabber grabber in grabbers)
+            {
+                grabber.ForceRelease(grabbable);
+            }
+            isSet = true;
+            transform.parent.position = other.transform.position;
+            transform.parent.rotation = other.transform.rotation;
+            transform.parent.GetComponent<OVRGrabbable>().enabled = false;
+            transform.parent.GetComponent<BoxCollider>().enabled = false;
+            transform.parent.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         }
     }
 
@@ -78,5 +114,24 @@ public class Claymore : MonoBehaviour
             other.transform.parent = null;
             audio.PlayOneShot(separate);
         }
+    }
+
+    private bool CheckClaymoreHidden()
+    {
+        for (int i = 0; i < rayPoints.Count; i++)
+        {
+            ray = new Ray(rayPoints[i].position, rayPoints[i].up);
+            Debug.DrawRay(rayPoints[i].position, rayPoints[i].up, Color.green, 0.4f);
+            if (Physics.Raycast(ray, out hit, 0.4f, leaf))
+            {
+                cylinders[i].enabled = false;
+            }
+            else
+            {
+                cylinders[i].enabled = true;
+                return false;
+            }
+        }
+        return true;
     }
 }
